f13d86b903d5a576180a4fb8a8609c823590e3ba
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-10-03 22:08:15 +0500

    Counter app in StatelessWidget, in StatelessWidget Widget are immutable they never change their state, so we achieved variable declation but still can't change its states

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:before_provider_using_setstate_rebuild_ui/home_screen_stateless.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Start with Provider State Management',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
